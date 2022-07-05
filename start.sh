7563a7144a1da2908623484cd2358ba2de6a6cf9
gpercsi <101331746+gpercsi@users.noreply.github.com>
2022-07-05 14:15:02 +0200

    Create start.sh
    start.sh különválasztás a Dockerfile-tól.
    
    TODO: SED-nek változó átadása a start.sh-n keresztül.

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+emqx start
+sed -i 's/node.name = emqx@127.0.0.1/node.name = emqx1@172.17.0.2/g' /etc/emqx/emqx.conf
+sed -i 's/cluster.discovery = manual/cluster.discovery = mcast\ncluster.mcast.addr = 239.192.0.1\ncluster.mcast.ports = 4369,4370\ncluster.mcast.iface = 0.0.0.0\ncluster.mcast.ttl = 255\ncluster.mcast.l>
+sed -i 's/node.cookie = emqxsecretcookie/node.cookie = sztakicluster/g' /etc/emqx/emqx.conf
+emqx restart
+sleep infinity
