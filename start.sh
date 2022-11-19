2ecc4301b50ad134dab48d6654f8412319ee54b8
gpercsi <101331746+gpercsi@users.noreply.github.com>
2022-11-19 12:09:36 +0100

    Update start.sh
    Insted of Ansible script, trying with bash script

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 emqx start
 sed -i 's/node.name = emqx@127.0.0.1/node.name = emqx'$ID'@'$IPADDRESS'/g' /etc/emqx/emqx.conf
-sed -i 's/cluster.discovery = manual/cluster.discovery = mcast\ncluster.mcast.addr = 239.192.0.1\ncluster.mcast.ports = 4369,4370\ncluster.mcast.iface = 0.0.0.0\ncluster.mcast.ttl = 255\ncluster.mcast.l>
+#sed -i 's/cluster.discovery = manual/cluster.discovery = mcast\ncluster.mcast.addr = 239.192.0.1\ncluster.mcast.ports = 4369,4370\ncluster.mcast.iface = 0.0.0.0\ncluster.mcast.ttl = 255\ncluster.mcast.l>
 sed -i 's/node.cookie = emqxsecretcookie/node.cookie = sztakicluster/g' /etc/emqx/emqx.conf
-emqx restart
+emqx stop
+emqx start
 sleep infinity
