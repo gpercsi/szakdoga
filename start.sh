5a1a70879077044316e98f20fab260689fce10d8
gpercsi <101331746+gpercsi@users.noreply.github.com>
2022-11-22 11:04:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Cluster name setup"
 sleep 45
 sed -i 's/node.name = emqx@127.0.0.1/node.name = emqx@'$IPADDRESS'/g' /etc/emqx/emqx.conf
 sed -i 's/dashboard.default_user.login = admin/dashboard.default_user.login = '$USERNAME'/g' /etc/emqx/plugins/emqx_dashboard.conf
-sed -i 's/dashboard.default_user.password = admin/dashboard.default_user.pawssword = '@PASSWORD'/g' /etc/emqx/plugins/emqx_dashboard.conf
+sed -i 's/dashboard.default_user.password = admin/dashboard.default_user.pawssword = '$PASSWORD'/g' /etc/emqx/plugins/emqx_dashboard.conf
 echo "Cluster cookie setup"
 sed -i 's/node.cookie = emqxsecretcookie/node.cookie = sztakicluster/g' /etc/emqx/emqx.conf
 echo "EMQX restart"
